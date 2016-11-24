c9504a4f970b1f1953a8e507ed728515176a5a24
Laura Abend <laura.l.abend@gmail.com>
2016-11-23 21:04:17 -0600

    add solo learn exercise file; using XAMPP

---
+++ soloLearnExercises.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Solo Learn PHP Lessons</title>
+  </head>
+  <body>
+    <?php
+      echo '<h1>Learning PHP</h1>';
+      echo 'Hello world - I am learning PHP using <strong>XAMPP</strong>!';
+     ?>
+  </body>
+</html>
