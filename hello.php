0a7fcb3e1f17f813d464bb68780b250a4a9b9697
Laura Abend <laura.l.abend@gmail.com>
2016-11-28 21:07:37 -0600

    add file for testing running a php program through CLI

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+// running a program from the CLI [found in PHP The Right Way]
+if ($argc !== 2) {
+    echo "Usage: php hello.php [name].\n";
+    exit(1);
+}
+$name = $argv[1];
+echo "Hello, $name\n";
+ ?>
