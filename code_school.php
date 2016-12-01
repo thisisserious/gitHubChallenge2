32bfad3ff605b23bf59bcaf32df8fed0aae7237b
Laura Abend <laura.l.abend@gmail.com>
2016-11-30 21:25:49 -0600

    add php file for code school exercises

---
+++ code_school.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'Laura Abend';
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Code School PHP Lessons</title>
+  </head>
+  <body>
+    <h1><?php echo $name; ?></h1>
+    <p><?php echo 'Hello world!'; ?></p>
+  </body>
+</html>
