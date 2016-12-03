03b1ea0d9f0d7da34954e595752c2c61ac15d189
Laura Abend <laura.l.abend@gmail.com>
2016-12-02 19:53:55 -0600

    add array practice

--- code_school.php
+++ code_school.php
@@ -1,5 +1,6 @@
 <?php
 $name = 'Laura Abend';
+$learning = array('PHP', 'JavaScript', 'Angular', 'HTML5', 'CSS3');
  ?>
 <!DOCTYPE html>
 <html>
@@ -10,6 +11,15 @@ $name = 'Laura Abend';
   <body>
     <h1><?php echo $name; ?></h1>
     <p><?php echo 'Hello world!'; ?></p>
+    <h3>I am learning:</h3>
+    <ul>
+      <li><?php echo $learning[0]; ?></li>
+      <li><?php echo $learning[1]; ?></li>
+      <li><?php echo $learning[2]; ?></li>
+      <li><?php echo $learning[3]; ?></li>
+      <li><?php echo $learning[4]; ?></li>
+    </ul>
+    <h3>...and so much more!</h3>
   </body>
   <footer><?php include 'footer.php'; ?></footer>
 </html>
