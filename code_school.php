9013971e48d7eb97167fcd3c4fba8b423b42757e
Laura Abend <laura.l.abend@gmail.com>
2016-12-12 19:37:53 -0600

    fix calculator functionality

--- code_school.php
+++ code_school.php
@@ -6,12 +6,7 @@ array_push($learning, 'Git & GitHub');
 array_push($learning, 'jQuery');
 $num1 = $_POST["num1"];
 $num2 = $_POST["num2"];
-$add = $_POST["add"];
-$subtract = $_POST["subtract"];
-$multiply = $_POST["multiply"];
-$divide = $_POST["divide"];
-// $mathOperation = $_POST["radio"];
-echo "<h1>$add</h1>";
+$mathOperation = $_POST["operator"];
  ?>
 <!DOCTYPE html>
 <html>
@@ -36,41 +31,44 @@ echo "<h1>$add</h1>";
       <!-- <li>This is the array that prints the above list items: <?php print_r($learning); ?></li> -->
     </ul>
     <h3>...and so much more!</h3>
+    <hr />
   </section>
-  <section>
+  <section id="calculator">
+    <h4>PHP Calculator</h4>
     <form method="post">
-      <input type="number" name="num1" />
-      <input type="number" name="num2" />
-      <div><input type="radio" name="add" />Add</div>
-      <div><input type="radio" name="subtract" />Subtract</div>
-      <div><input type="radio" name="multiply" />Multiply</div>
-      <div><input type="radio" name="divide" />Divide</div>
+      <label for="value1">Value 1: </label><input type="number" name="num1" />
+      <div><input type="radio" name="operator" value="add" />+</div>
+      <div><input type="radio" name="operator" value="subtract" />-</div>
+      <div><input type="radio" name="operator" value="multiply" />*</div>
+      <div><input type="radio" name="operator" value="divide" />/</div>
+      <label for="value2">Value 2: </label><input type="number" name="num2" />
       <input type="submit" name="calculate" />
     </form>
     <p>1st number: <?php echo $num1; ?></p>
     <p>2nd number: <?php echo $num2; ?></p>
     <?php
-    switch($mathOperation):
-      case $add:
+    switch ($mathOperation):
+      case "add":
         $sum = $num1 + $num2;
-        echo "<p>$sum</p>";
+        echo "<p>Equals: $sum</p>";
         break;
-      case $subtract:
+      case "subtract":
         $diff = $num1 - $num2;
-        echo "<p>$diff</p>";
+        echo "<p>Equals: $diff</p>";
         break;
-      case $multiply:
+      case "multiply":
         $factor = $num1 * $num2;
-        echo "<p>$factor</p>";
+        echo "<p>Equals: $factor</p>";
         break;
-      case $divide:
+      case "divide":
         $quotient = $num1 / $num2;
-        echo "<p>$quotient</p>";
+        echo "<p>Equals: $quotient</p>";
         break;
       default:
         echo "<p>None of the above.</p>";
     endswitch;
      ?>
+     <hr />
   </section>
   </main>
   </body>
