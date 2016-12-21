58c77fa63ffea68ebb75149a671fae19aef31c76
Laura Abend <laura.l.abend@gmail.com>
2016-12-21 17:57:45 -0600

    add beginning of php todo list

---
+++ todo.php
@@ -0,0 +1,28 @@
+<?php
+$tasks = array();
+$newTask = $_POST["task"];
+array_push($tasks, $newTask);
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP To Do List</title>
+  </head>
+  <body>
+    <main>
+      <h1>To Do List in <em>PHP</em></h1>
+      <form method="post">
+        <label for="task">Add a new task: </label><input type="text" name="task" />
+        <input type="submit" name="addTask" />
+      </form>
+      <ul>
+        <?php
+          foreach ($tasks as $todo) {
+          echo "<li>$todo</li>";
+        }
+         ?>
+      </ul>
+    </main>
+  </body>
+</html>
